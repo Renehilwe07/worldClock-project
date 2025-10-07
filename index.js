b5be7629f0f146a9494405228b51142f5ec9c3b7
Renehilwe M <renehilwec07@gmail.com>
2025-10-07 05:36:11 +0200

    added a link to home page

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ function updateCity(event) {
         </div>
         <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
     </div>
-
-    `
+    <a href="/">All cities</a>
+`
 }
  updateTime();
  setInterval(updateTime, 1000);
